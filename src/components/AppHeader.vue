f66ee89c62442c01a5b65e03be0b4897089b8c3b
moreDasha <moredasha@icloud.com>
2025-02-07 14:34:10 +0400

    init: init project

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface IMenuItem {
+  label: string,
+  icon: string,
+  path: string
+}
+
+const items = ref<IMenuItem[]>([
+  {
+    label: 'Авторизация',
+    icon: 'pi pi-user',
+    path: '/auth'
+  },
+  {
+    label: 'Добавить',
+    icon: 'pi pi-plus',
+    path: '/'
+  },
+  {
+    label: 'Список собеседований',
+    icon: 'pi pi-list',
+    path: '/list'
+  },
+  {
+    label: 'Статистика',
+    icon: 'pi pi-chart-pie',
+    path: '/statistic'
+  }
+]);
+</script>
+<template>
+  <app-menubar :model="items" class="app-menu">
+    <template #item="{ item, props }">
+      <router-link :to="item.path" v-bind="props.action" class="flex align-items-center">
+        <span :class="item.icon" class="p-menuitem-icon"></span>
+        <span class="ml-2">{{ item.label }}</span>
+      </router-link>
+    </template>
+  </app-menubar>
+</template>
+
+<style scoped>
+.app-menu {
+  margin: 30px 0;
+}
+</style>
